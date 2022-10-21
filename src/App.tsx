72c243533630ef6d70cf782b45e7b9439bed792a
Ada <me@helloding.cn>
2022-10-21 10:21:57 +0800

    feat: add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import FoodList from "./pages/foodList";
 import Order from "./pages/order";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
       <Routes>
           <Route path="/" element={<FoodList />}/>
           <Route path="/order" element={<Order />}/>
+          <Route path="*" element={<NotFound />} />
       </Routes>
   );
 }
